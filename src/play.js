743216646ac0ea5d1969049749cd317e266f3f9f
alansmart <sharma.alankar831@gmail.com>
2021-05-05 16:00:33 +0530

    Updated js files

--- src/play.js
+++ src/play.js
@@ -1,3 +1,6 @@
-import {add} from './method';
+import {shuffle, add, sort} from './method';
+
+document.getElementById('shuffleBtn').addEventListener('click', shuffle);
+document.getElementById('sortBtn').addEventListener('click', sort);
 
 console.log(add(5,10));
